abc624b3ffd08677788ce629f228a8fa6e23cf01
Kane0086 <bassagal.louis@gmail.com>
2023-12-02 13:48:46 +0100

    [ADD] Add something

--- Assets/Components/Mario/Scripts/PlayerController.cs
+++ Assets/Components/Mario/Scripts/PlayerController.cs
@@ -7,12 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5F;
     public float jumpForce = 10F;
+    public int healthPoints = 3;
 
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
     private bool _isGrounded = true;
-    
+    private float _timer;
+    private float _invincibleTimer;
+    private bool _isInvincible = false;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -25,6 +28,7 @@ public class PlayerController : MonoBehaviour
     {
         Moving();
         Jumping();
+        Invincibility();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -32,6 +36,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && !_isInvincible)
+        {
+            print("Here");
+            healthPoints -= 1;
+            _isInvincible = true;
+            _timer = 0;
         }
     }
 
@@ -64,5 +79,25 @@ public class PlayerController : MonoBehaviour
         _animator.SetBool("IsJumping", true);
         _isGrounded = false;
         _rb.velocity = new Vector2(velocity.x, jumpForce);
+    }
+
+    private void Invincibility()
+    {
+        _timer += Time.deltaTime;
+        _invincibleTimer += Time.deltaTime;
+        if (!_isInvincible)
+            return;
+        if (_timer > 1F)
+        {
+            _isInvincible = false;
+            _spriteRenderer.color = new Color(255, 255, 255, 255);
+            return;
+        }
+
+        if (_invincibleTimer < 0.1F)
+            return;
+        print("HERE2");
+        _spriteRenderer.color = new Color(255, 255, 255, _spriteRenderer.color.a == 1 ? 0.5F : 1);
+        _invincibleTimer = 0;
     }
 }
